cf1c9827c98c355078f7e52328a1da21f4e5d7b3
sreekanth_k <sreekanth100k@gmail.com>
2020-01-14 20:01:56 +0530

    terms and services color changed

--- Demo1/app/src/main/java/com/example/demo1/NewRegistrationProfessionalActivity.java
+++ Demo1/app/src/main/java/com/example/demo1/NewRegistrationProfessionalActivity.java
@@ -4,16 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Html;
 import android.text.Spannable;
 import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
-public class NewRegistrationProfessional extends AppCompatActivity {
+public class NewRegistrationProfessionalActivity extends AppCompatActivity {
 
     private TextView mTermsNServicesTv;
 
@@ -36,6 +42,31 @@ public class NewRegistrationProfessional extends AppCompatActivity {
         getReferenceOfViews();
 
         mTermsOfServiceNprivacyPolicyText   =  getResources().getString(R.string.terms_privacy_policy);
+
+        SpannableString ss = new SpannableString(mTermsOfServiceNprivacyPolicyText);
+        ClickableSpan clickableTerms = new ClickableSpan() {
+            @Override
+            public void onClick(View textView) {
+                // show toast here
+
+                Toast.makeText(getApplicationContext(),"Terms of service clicked",Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                super.updateDrawState(ds);
+
+            }
+        };
+        ss.setSpan(new ForegroundColorSpan(Color.BLUE), 29, 45, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+
+        ss.setSpan(new ForegroundColorSpan(Color.BLUE), 47,ss.length() , Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+
+        mTermsNServicesTv.setText(ss);
+        mTermsNServicesTv.setMovementMethod(LinkMovementMethod.getInstance());
+        mTermsNServicesTv.setHighlightColor(Color.TRANSPARENT);
+
 
         mSignUpBtn.setOnClickListener(new View.OnClickListener(){
 
