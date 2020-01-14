3f0f1e9715573ba93e5ea552734dad855bb0284c
sreekanth_k <sreekanth100k@gmail.com>
2020-01-14 19:02:50 +0530

    Action bar is now hidden in the login activity

--- Demo1/app/src/main/java/com/example/demo1/NewRegistrationProfessional.java
+++ Demo1/app/src/main/java/com/example/demo1/NewRegistrationProfessional.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -17,12 +19,12 @@ public class NewRegistrationProfessional extends AppCompatActivity {
 
     private String mTermsOfServiceNprivacyPolicyText;
 
-
+    private Button mSignUpBtn;
 
 
     public void getReferenceOfViews(){
         mTermsNServicesTv = (TextView) findViewById(R.id.id_terms_privacy_policy_tv);
-
+        mSignUpBtn        = (Button) findViewById(R.id.id_sign_up_btn);
 
     }
     @Override
@@ -30,26 +32,18 @@ public class NewRegistrationProfessional extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_registration_professional);
 
+        getSupportActionBar().hide();
         getReferenceOfViews();
 
         mTermsOfServiceNprivacyPolicyText   =  getResources().getString(R.string.terms_privacy_policy);
 
+        mSignUpBtn.setOnClickListener(new View.OnClickListener(){
 
+            @Override
+            public void onClick(View v) {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            }
+        });
 
 
     }
