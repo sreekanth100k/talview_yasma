1d2fc2a2f9db37449a0539b30f5861a4d615da5b
sreekanth_k <sreekanth100k@gmail.com>
2020-01-14 17:49:30 +0530

    Initial commit on registration professional

--- Demo1/app/src/main/java/com/example/demo1/NewRegistrationProfessional.java
+++ Demo1/app/src/main/java/com/example/demo1/NewRegistrationProfessional.java
@@ -3,11 +3,30 @@ package com.example.demo1;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class NewRegistrationProfessional extends AppCompatActivity {
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.new_registration_professional);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
